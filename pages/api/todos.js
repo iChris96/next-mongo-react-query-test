f9f3b7b6b3b0abff5f6fe21b13057f2c5bedef8c
iChris96 <christopher_x10x@hotmail.com>
2022-07-22 00:40:56 -0500

    fix: todo was not successfully saved after toggle in his state

--- pages/api/todos.js
+++ pages/api/todos.js
@@ -1,42 +1,42 @@
 import connectDB from "/middleware/mongodb";
-import {Todos} from "/schemas/todos.js"
+import { Todos } from "/schemas/todos.js"
 
 const getTodos = async (req, res) => {
   const todos = await Todos.find()
-  console.log({todos})
+  console.log({ todos })
   return res.status(200).json({ todos })
 }
 
 const addTodo = async (req, res) => {
-  const { title, isCompleted} = req.body;
-  const result = await Todos.create({ title, isCompleted})
-  console.log({result})
+  const { title, isCompleted } = req.body;
+  const result = await Todos.create({ title, isCompleted })
+  console.log({ result })
   return res.status(200).json({ status: 'ok' })
 }
 
 const uptateTodo = async (req, res) => {
   const { todo } = req.body;
-  const currentTodo = await Todos.findOneAndUpdate({id: todo._id}, { title: todo.title, isCompleted: todo.isCompleted});
-  const updatedTodo = await Todos.findOne({id: todo._id});
-  console.log({currentTodo, updatedTodo})
+  const currentTodo = await Todos.findOneAndUpdate({ _id: todo._id }, { title: todo.title, isCompleted: todo.isCompleted });
+  const updatedTodo = await Todos.findOne({ _id: todo._id });
+  console.log({ currentTodo, updatedTodo })
   return res.status(200).json({ status: 'ok', message: 'TODO updated', updatedTodo })
 }
 
-const handleTodos =  async (req,res) => {
+const handleTodos = async (req, res) => {
   const method = req.method;
 
   try {
-    if(method == 'GET') return getTodos(req,res)
-    if(method == 'POST') return addTodo(req,res)
-    if(method == 'PUT') return uptateTodo(req,res)
+    if (method == 'GET') return getTodos(req, res)
+    if (method == 'POST') return addTodo(req, res)
+    if (method == 'PUT') return uptateTodo(req, res)
   } catch (error) {
     console.error('Server Error: ', error)
     return res.status(500)
   }
 
-  
-  
-  return res.status(404).json('No Found') 
+
+
+  return res.status(404).json('No Found')
 }
 
 
